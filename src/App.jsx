75dcb0ab65599b4d90c576062a3f9b946beb2176
E O <114992756+Emmanuel-Awolu60@users.noreply.github.com>
2023-11-21 20:37:04 +0100

    Added a residenies file

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ const App = () => {
         <Hero />
       </div>
       <Companies />
+      <Residencies />
     </div>
   );
 };
--- src/components/Residencies/Residencies.jsx
+++ src/components/Residencies/Residencies.jsx
@@ -1,14 +1,35 @@
-import React from 'react'
-// import {Swiper, SwiperSlide, useSwiper} from "swiper/react"
-// import "swiper"
-import "./Residencies.css"
+import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "./Residencies.css";
+import "../../utils/slider.json";
 
 const Residencies = () => {
   return (
-    <div>
-      {/* ,opfinbviofnvibvfionoj */}
-    </div>
-  )
-}
+    <section className="r-wrapper">
+      <div className="paddings innerWidth r-container">
+        <div className="r-head flexColStart">
+          <span className="orangeText">Best Choices</span>
+          <span className="primaryText">Popular Residencies</span>
+        </div>
 
-export default Residencies
+        <Swiper>
+          {data.map((card, i) => {
+            <SwiperSlide key={i}>
+              <div className="r-card">
+                <img src={card.image} alt="home" />
+
+                <span className="secondaryText r-price">
+                  <span>$</span>
+                  <span>{card.price}</span>
+                </span>
+              </div>
+            </SwiperSlide>;
+          })}
+        </Swiper>
+      </div>
+    </section>
+  );
+};
+
+export default Residencies;
