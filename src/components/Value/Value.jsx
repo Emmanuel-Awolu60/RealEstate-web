320e92f9816a0274ea1007a9186dde602b5cc5e3
E O <114992756+Emmanuel-Awolu60@users.noreply.github.com>
2023-11-24 15:18:04 +0100

    updated

--- src/components/Value/Value.jsx
+++ src/components/Value/Value.jsx
@@ -1,17 +1,18 @@
 import React from 'react'
-import{
+import {
     Accordion,
     AccordionItem,
     AccordionItemHeading,
     AccordionItemButton,
     AccordionItemPanel,
-    AccordionItemState,
-} from 'react-accessible-accordion'
-
+} from 'react-accessible-accordion';
+import 'react-accessible-accordion/dist/fancy-example.css';
+import{MdOutlineArrowDropDown} from 'react-icons/md'
+import './Value.css'
 const Value = () => {
   return (
     <div>
-      
+     
     </div>
   )
 }
